aa81b9a8072927b4e84fa5c9a013972ca26f1cb6
Sinan Gunes <gunes.sinan@gmail.com>
2018-01-15 23:37:33 +0300

    Beta 0 on play store

--- app/src/main/java/com/guness/elevator/db/AppDatabase.java
+++ app/src/main/java/com/guness/elevator/db/AppDatabase.java
@@ -3,7 +3,7 @@ package com.guness.elevator.db;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-@Database(entities = {ElevatorEntity.class, GroupEntity.class, SettingsEntity.class, FavoriteEntity.class, PanelPrefsEntity.class}, version = 11)
+@Database(entities = {ElevatorEntity.class, GroupEntity.class, SettingsEntity.class, FavoriteEntity.class, PanelPrefsEntity.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract AppDao dao();
 }
